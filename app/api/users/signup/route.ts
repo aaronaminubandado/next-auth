9ec13db4cdbfcada8b4d17e1d8ed2ea2b764e337
badguybandi <aaron.aminu1@gmail.com>
2024-07-22 14:31:59 +0300

    Created a route for email verification

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/app/lib/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { error } from "console";
+import { sendEmail } from "@/app/helpers/mailer";
 
 connect();
 
@@ -34,6 +34,11 @@ export async function POST(request: NextRequest) {
 
         //save user to database
         const savedUser = await newUser.save();
+        await sendEmail({
+            email, 
+            emailType:"VERIFY",
+            userId: savedUser._id
+        });
 
         return NextResponse.json({
             message: "User created successfully",
