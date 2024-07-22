17b0a0aae802085a29a46c9f9b909e6613f3255f
badguybandi <aaron.aminu1@gmail.com>
2024-07-22 15:26:47 +0300

    made changes to the signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -16,43 +16,50 @@ export default function SignUpPage() {
     const onSignUp = async () => {
         try {
             const response = await axios.post("/api/users/signup", user);
+            console.log("Signup response", response);
             router.push("/login");
-        } catch (error) {
-            console.log("Signup failed", error);   
+        } catch (error:any) {
+            console.log("Signup failed", error.response ? error.response.data : error.message);
         }
-    }
+    };
+    
 
     return (
         <div>
-            <label htmlFor="username">Username</label>
-            <input
-                id="username" 
-                type="text" 
-                value={user.username}
-                onChange={(e) => setUser({...user, username: e.target.value})}
-                placeholder="Username"
-            />
+            <form>
+                <label htmlFor="username">Username</label>
+                <input
+                    id="username" 
+                    type="text" 
+                    value={user.username}
+                    onChange={(e) => setUser({...user, username: e.target.value})}
+                    placeholder="Username"
+                />
 
-            <label htmlFor="email">Email</label>
-            <input 
-                id="email"
-                type="text"
-                value={user.email}
-                onChange={(e) => setUser({...user, email: e.target.value})}
-                placeholder="user@formmail.com" 
-            />
+                <label htmlFor="email">Email</label>
+                <input 
+                    id="email"
+                    type="text"
+                    value={user.email}
+                    onChange={(e) => setUser({...user, email: e.target.value})}
+                    placeholder="user@formmail.com" 
+                />
 
-            <label htmlFor="password">Password</label>
-            <input 
-                id="password"
-                type="password"
-                value={user.password}
-                onChange={(e) => setUser({...user, password: e.target.value})}
-                placeholder="password" 
-            />
+                <label htmlFor="password">Password</label>
+                <input 
+                    id="password"
+                    type="password"
+                    value={user.password}
+                    onChange={(e) => setUser({...user, password: e.target.value})}
+                    placeholder="password" 
+                />
 
-            <button onClick={onSignUp}>Sign Up</button>
-
+                <button className="" onClick={(e) => {
+                    e.preventDefault();
+                    onSignUp;
+                }}>Sign Up</button>
+            </form>
+            <hr/>
             <Link href="/login">Visit Login Page</Link>
         </div>
     );
